e06575ef286fefa17bdc6116229165142e130cdb
Hsuan Lee <boczeratul@gmail.com>
2020-09-23 09:14:47 +0800

    Update to fit fcl@0.0.56

--- src/demo/Authenticate.js
+++ src/demo/Authenticate.js
@@ -1,16 +1,7 @@
 import React, {useState, useEffect} from "react"
-import styled from "styled-components"
 import * as fcl from "@onflow/fcl"
 
 import Card from '../components/Card'
-
-const Profile = styled.div`
-  margin-bottom: 10px;
-`
-const Img = styled.img`
-  width: 50px;
-  height: 50px;
-`
 
 const SignInOutButton = ({ user: { loggedIn } }) => {
   const signInOrOut = async (event) => {
@@ -30,19 +21,6 @@ const SignInOutButton = ({ user: { loggedIn } }) => {
   )
 }
 
-const UserProfile = ({ user }) => (
-  <Profile>
-    {user.identity.avatar && <Img src={user.identity.avatar} />}
-
-    <div>
-      <b>Name</b>: {user.identity.name || "Anonymous"}
-    </div>
-    <div>
-      <b>Address</b>: {user.addr || ""}
-    </div>
-  </Profile>
-)
-
 const CurrentUser = () => {
   const [user, setUser] = useState({})
 
@@ -54,8 +32,6 @@ const CurrentUser = () => {
 
   return (
     <Card>
-      {user.loggedIn && <UserProfile user={user} />}
-
       <SignInOutButton user={user} />
     </Card>
   )
