597e8ccb16122265552f44f23045290209f215f8
Hsuan Lee <boczeratul@gmail.com>
2021-06-09 19:52:00 +0800

    Fix configs

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import * as fcl from "@onflow/fcl"
 
 fcl.config()
   .put("accessNode.api", "http://localhost:8080") // local Flow emulator
-  .put("challenge.handshake", "http://localhost:8702/authn") // local dev wallet
+  .put("challenge.handshake", "http://localhost:8701/flow/authenticate") // local dev wallet
   .put("challenge.scope", "email") // request for Email
   // .put("accessNode.api", "https://access-testnet.onflow.org") // Flow testnet
   // .put("challenge.handshake", "https://flow-wallet-testnet.blocto.app/authn") // Blocto testnet wallet
