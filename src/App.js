99eb6d3d33535884118bc3d3ccbb12a3c67a8a29
Hsuan Lee <boczeratul@gmail.com>
2021-10-05 14:40:40 +0800

    Fix lint

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import styled from 'styled-components'
 import Section from './components/Section'
 import Header from './components/Header'
 
-import GetLatestBlock from './demo/GetLatestBlock'
-import GetAccount from './demo/GetAccount'
-import ScriptOne from "./demo/ScriptOne"
-import ScriptTwo from './demo/ScriptTwo'
+// import GetLatestBlock from './demo/GetLatestBlock'
+// import GetAccount from './demo/GetAccount'
+// import ScriptOne from "./demo/ScriptOne"
+// import ScriptTwo from './demo/ScriptTwo'
 import Authenticate from './demo/Authenticate'
 import UserInfo from './demo/UserInfo'
 import SendTransaction from './demo/SendTransaction'
-import SignMessage from './demo/SignMessage'
-import SendFUSD from './demo/SendFUSD'
-import DeployContract from './demo/DeployContract'
-import InteractWithContract from './demo/InteractWithContract'
+// import SignMessage from './demo/SignMessage'
+// import SendFUSD from './demo/SendFUSD'
+// import DeployContract from './demo/DeployContract'
+// import InteractWithContract from './demo/InteractWithContract'
 
 const Wrapper = styled.div`
   font-size: 13px;
