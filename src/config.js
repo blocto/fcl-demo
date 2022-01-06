27d0f19e9c74e70d96d32bf41e7b8b28960a8f42
Hsuan Lee <boczeratul@gmail.com>
2022-01-06 09:26:19 +0800

    Use non https

--- src/config.js
+++ src/config.js
@@ -5,6 +5,5 @@ fcl.config()
   // .put("challenge.handshake", "http://localhost:8701/flow/authenticate") // local dev wallet
   .put("challenge.scope", "email") // request for Email
   .put("accessNode.api", "https://access-testnet.onflow.org") // Flow testnet
-  .put("discovery.wallet", "https://flow-wallet-testnet.blocto.app/api/flow/authn") // Blocto testnet wallet
-  .put("discovery.wallet.method", "HTTP/POST")
+  .put("discovery.wallet", "https://flow-wallet-testnet.blocto.app/authn") // Blocto testnet wallet
   .put("service.OpenID.scopes", "email!")
