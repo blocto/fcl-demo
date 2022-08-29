6c2f962e91217a2663ff56f7f77e4e7581eb7765
Hsuan Lee <boczeratul@gmail.com>
2022-08-29 10:09:54 +0800

    Update to HTTP/POST

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ fcl.config()
   // .put("challenge.handshake", "http://localhost:8701/flow/authenticate") // local dev wallet
   .put("challenge.scope", "email") // request for Email
   .put("accessNode.api", "https://access-testnet.onflow.org") // Flow testnet
-  .put("challenge.handshake", "https://flow-wallet-testnet.blocto.app/authn") // Blocto testnet wallet
-  .put("discovery.wallet.method", "TAB/RPC")
+  .put("challenge.handshake", "https://flow-wallet-testnet.blocto.app/api/flow/authn") // Blocto testnet wallet
+  .put("discovery.wallet.method", "HTTP/POST")
