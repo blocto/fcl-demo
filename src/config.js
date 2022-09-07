1cbb2d3829a02d1f06ffae80f94d63358eed2193
Hsuan Lee <boczeratul@gmail.com>
2022-09-07 20:15:56 +0800

    Update access node

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ fcl.config()
   // .put("accessNode.api", "http://localhost:8080") // local Flow emulator
   // .put("challenge.handshake", "http://localhost:8701/flow/authenticate") // local dev wallet
   .put("challenge.scope", "email") // request for Email
-  .put("accessNode.api", "https://access-testnet.onflow.org") // Flow testnet
+  .put("accessNode.api", "https://rest-testnet.onflow.org") // Flow testnet
   .put("discovery.wallet", "https://flow-wallet-testnet.blocto.app/authn") // Blocto testnet wallet
   .put("service.OpenID.scopes", "email!")
